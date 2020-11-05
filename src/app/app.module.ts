1b01b2b0c41176d357e8b02931493c06eb25dfa1
Gijsbert ter Horst <gijsbert.terhorst@intar.nl>
2020-11-05 10:27:44 +0100

    Demonstrate https://github.com/apache/incubator-echarts/issues/13541

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import * as echarts from 'echarts';
+import { NgxEchartsModule } from 'ngx-echarts';
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxEchartsModule.forRoot({echarts}),
   ],
   providers: [],
   bootstrap: [AppComponent]
