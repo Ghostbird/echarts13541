a986d652e05151a932740ad92906f3217f239c28
Gijsbert ter Horst <gijsbert.terhorst@intar.nl>
2020-11-16 14:40:32 +0100

    Regenerate, drop tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'echarts135410demo';
+  title = 'echarts13541';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import * as echarts from 'echarts';
-import { NgxEchartsModule } from 'ngx-echarts';
+
 import { AppComponent } from './app.component';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    NgxEchartsModule.forRoot({echarts}),
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
