0e23d39b11fc96105295804b4d841cdb65237e3b
Gijsbert ter Horst <gijsbert.terhorst@intar.nl>
2020-11-23 12:37:37 +0100

    Illustrate not exported issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CallbackDataParams } from 'echarts/types/dist/echarts';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'echarts13541';
+  constructor() {
+    const labelFormatter = (info: CallBackDataParams) => '<div>Test</div>';
+  }
 }
